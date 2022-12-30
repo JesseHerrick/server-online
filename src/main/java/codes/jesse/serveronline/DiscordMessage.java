f9767c35fa4b558ff321fa5ea8a28bbf30b08cdc
Jesse Herrick <me@jesseherrick.com>
2022-12-29 20:05:11 -0500

    Remove unused import

--- src/main/java/codes/jesse/serveronline/DiscordMessage.java
+++ src/main/java/codes/jesse/serveronline/DiscordMessage.java
@@ -6,7 +6,6 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.logging.Logger;
 
 public class DiscordMessage {
     public URI discordWebhookUri;
