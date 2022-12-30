c930bbc93c49b12db3e4b54c75e86914b8cdeecb
Jesse Herrick <me@jesseherrick.com>
2022-12-29 20:14:55 -0500

    Formatting

--- src/main/java/codes/jesse/serveronline/ServerOnline.java
+++ src/main/java/codes/jesse/serveronline/ServerOnline.java
@@ -3,7 +3,6 @@ package codes.jesse.serveronline;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ServerOnline extends JavaPlugin {
-
     @Override
     public void onEnable() {
         saveDefaultConfig();
